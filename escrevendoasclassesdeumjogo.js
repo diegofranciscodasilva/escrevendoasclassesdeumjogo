2a1c63d2bbc5336c0c28ee75c0d4c6db74a551b4
diegofranciscodasilva <diegofranciscodasilva@yahoo.com.br>
2023-12-22 14:42:25 -0300

    escrevendo classes de um jogo

---
+++ escrevendoasclassesdeumjogo.js
@@ -0,0 +1,36 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+      this.nome = nome
+      this.idade = idade
+      this.tipo = tipo
+  }
+
+  atacar() {
+      let ataque
+      switch (this.tipo) {
+          case 'mago':
+              ataque = 'usou magia'
+              break
+          case 'guerreiro':
+              ataque = 'usou espada'
+              break
+          case 'monge':
+              ataque = 'usou artes marciais'
+              break
+          case 'ninja':
+              ataque = 'usou shuriken'
+              break
+          default:
+              ataque = 'usou um ataque desconhecido'
+      }
+
+      const mensagem = `O ${this.tipo} ${this.nome} atacou usando ${ataque}`
+      console.log(mensagem)
+  }
+}
+
+const heroi1 = new Heroi('Aragorn', 30, 'guerreiro')
+const heroi2 = new Heroi('Gandalf', 100, 'mago')
+
+heroi1.atacar()
+heroi2.atacar()
